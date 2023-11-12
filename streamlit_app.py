f4ea40d24082e24130300866ae704005955a6c34
ananthks <150622308+an-antks@users.noreply.github.com>
2023-11-12 15:44:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import streamlit
 streamlit.title('|| My Parents New Healthier Diner ||')
 
 streamlit.header('Breakfast Menu')
-
-streamlit.text('Idly Vada  |  Dose  |  Uppittu')
+streamlit.text('Idly Vade  |  Dwaase  |  Uppittu')
 streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
