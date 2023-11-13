f60cb4de0c43ece0ca49c50f81fc16b2a0b0fc70
ananthks <150622308+an-antks@users.noreply.github.com>
2023-11-13 18:58:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('|| My Parents New Healthier Diner ||')
+streamlit.title('|| My New Healthier Diner ||')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Idly Vade  |  Dwaase  |  Uppittu')
